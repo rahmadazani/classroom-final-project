365cf23bef391255371795c491e0ce4e9096e036
Rahmad <azanirahmad51@gmail.com>
2021-08-20 00:13:29 +0700

    classroom awal banget

---
+++ pages/classroom.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>LIST CLASS</h3>
+    <div class="row">
+      <div class="col-4">
+        <card />
+      </div>
+      <div class="col-4">
+        <card />
+      </div>
+      <div class="col-4">
+        <card />
+      </div>
+    </div>
+  </div>
+</template>

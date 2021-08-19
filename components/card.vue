6bbce91247d28738f11906eb7e6a8ea3ad0e7ccf
Rahmad <azanirahmad51@gmail.com>
2021-08-19 23:35:50 +0700

    partial

---
+++ components/card.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="card">
+  <img src="~/assets/img/unsplash/andre-benz-1214056-unsplash.jpg" class="card-img-top" alt="gambar kelas">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</template>

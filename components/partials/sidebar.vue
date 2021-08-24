e2efe12786dfe7c5546d93f880e6f201e6f5f869
Rahmad <azanirahmad51@gmail.com>
2021-08-24 12:41:35 +0700

    selasa sidebar

--- components/partials/sidebar.vue
+++ components/partials/sidebar.vue
@@ -8,19 +8,10 @@
         <a href="index.html">St</a>
       </div>
       <ul class="sidebar-menu">
-        <li class="menu-header">Dashboard</li>
-        <li class="nav-item dropdown">
-          <a href="#" class="nav-link has-dropdown"
+        <li>
+          <a href="#" class="nav-link"
             ><i class="fas fa-fire"></i><span>Dashboard</span></a
           >
-          <ul class="dropdown-menu">
-            <li>
-              <a class="nav-link" href="index-0.html">General Dashboard</a>
-            </li>
-            <li>
-              <a class="nav-link" href="index.html">Ecommerce Dashboard</a>
-            </li>
-          </ul>
         </li>
       </ul>
     </aside>
@@ -29,7 +20,9 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      menu: [],
+    };
   },
 };
 </script>
